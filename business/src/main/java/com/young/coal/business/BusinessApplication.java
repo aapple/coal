11c47b7883440b30f8d5c9c927ef4f763183636c
bin.yao <bin.yao@yeahmobi.com>
2018-07-26 15:35:12 +0800

    Initial commit

---
+++ business/src/main/java/com/young/coal/business/BusinessApplication.java
@@ -0,0 +1,9 @@
+package com.young.coal.business;
+
+import org.springframework.boot.SpringApplication;
+
+public class BusinessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApplicationConfig.class, args);
+    }
+}
