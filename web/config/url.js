1a4539a5a89a67f2640326440df5677b1004b44a
bin.yao <bin.yao@yeahmobi.com>
2018-08-03 17:27:14 +0800

    配置admin打包参数

--- web/config/url.js
+++ web/config/url.js
@@ -1,6 +1,6 @@
 import env from './env'
 
 const DEV_URL = 'http://localhost:8089/'
-const PRO_URL = 'https://produce.com'
+const PRO_URL = 'http://119.29.250.146:84'
 
 export default env === 'development' ? DEV_URL : PRO_URL
